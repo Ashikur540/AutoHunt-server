831a5e060885fd148f9f20b39a11c16f4da7c2fd
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-11-25 18:55:32 +0600

    purchase car api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient, ObjectId, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.port || 5000
 const app = express();
@@ -12,24 +12,68 @@ app.use(express.json());
 
 
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@cluster0.6vknfdj.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri);
-
-
-// // load all appointments data
-// const DBConnect = async () => {
-//     try {
-//         client.connect();
-//         console.log("success connection");
-//     } catch (error) {
-//         console.log(error.message);
-//     }
-// }
-
-// DBConnect();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@cluster0.6vknfdj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// console.log(uri);
 
 
 
+// load all appointments data
+const DBConnect = async () => {
+    try {
+        client.connect();
+        console.log("success connection");
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+DBConnect();
+
+// -------------------------collections---------------------------------
+const usersCollection = client.db('AutoHunt').collection('users');
+const carsCollection = client.db('AutoHunt').collection('cars');
+const categoryCollection = client.db('AutoHunt').collection('category');
+const carPurchaseCollection = client.db('AutoHunt').collection('purchase');
+
+
+
+
+
+
+/* ################MY MiddleWares  ########################*/
+
+
+
+
+
+
+/* ################MY MiddleWares  ########################*/
+
+
+
+
+
+/* ################MY get   ########################*/
+// load all cars  based on company
+app.get('/cars', async (req, res) => {
+    const { category_name } = req.query;
+    // console.log(category_name);
+    const result = await carsCollection.find({ category_name: category_name }).toArray();
+
+    res.send(result)
+})
+
+// category load
+app.get('/category', async (req, res) => {
+    const result = await categoryCollection.find({}).toArray();
+    res.send(result);
+})
+
+
+
+
+/* ################MY get  ########################*/
 
 
 
@@ -38,8 +82,24 @@ app.use(express.json());
 
 
 
+/* ################MY post   ########################*/
+
+app.post('/user', async (req, res) => {
+    const userInfo = req.body;
+    const result = await usersCollection.insertOne(userInfo);
+    res.send(result)
+})
+app.post('/purchase', async (req, res) => {
+    const purchaseInfo = req.body;
+    const result = await carPurchaseCollection.insertOne(purchaseInfo);
+    res.send(result);
+})
 
 
+
+
+
+/* ################MY post  ########################*/
 
 
 
